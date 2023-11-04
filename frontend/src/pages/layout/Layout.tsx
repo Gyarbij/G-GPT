649e2ab7d50a222d44042bb70e1e1d6737dae164
Gyarbij (Chono N) <49493993+Gyarbij@users.noreply.github.com>
2023-11-04 20:35:10 +0100

    New header

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -82,54 +82,9 @@ const Layout = () => {
                             <h1 className={styles.headerTitle}>DoROAD AI</h1>
                         </Link>
                     </Stack>
-                    <Stack horizontal tokens={{ childrenGap: 4 }}>
-                            {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
-                                <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
-                            }
-                            {/* <ShareButton onClick={handleShareClick} /> */}
-                    </Stack>
 
                 </Stack>
             </header>
-            <Outlet />
-            <Dialog 
-                onDismiss={handleSharePanelDismiss}
-                hidden={!isSharePanelOpen}
-                styles={{
-                    
-                    main: [{
-                        selectors: {
-                          ['@media (min-width: 480px)']: {
-                            maxWidth: '600px',
-                            background: "#FFFFFF",
-                            boxShadow: "0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)",
-                            borderRadius: "8px",
-                            maxHeight: '200px',
-                            minHeight: '100px',
-                          }
-                        }
-                      }]
-                }}
-                dialogContentProps={{
-                    title: "Share the web app",
-                    showCloseButton: true
-                }}
-            >
-                <Stack horizontal verticalAlign="center" style={{gap: "8px"}}>
-                    <TextField className={styles.urlTextBox} defaultValue={window.location.href} readOnly/>
-                    <div 
-                        className={styles.copyButtonContainer} 
-                        role="button" 
-                        tabIndex={0} 
-                        aria-label="Copy" 
-                        onClick={handleCopyClick}
-                        onKeyDown={e => e.key === "Enter" || e.key === " " ? handleCopyClick() : null}
-                    >
-                        <CopyRegular className={styles.copyButton} />
-                        <span className={styles.copyButtonText}>{copyText}</span>
-                    </div>
-                </Stack>
-            </Dialog>
         </div>
     );
 };
